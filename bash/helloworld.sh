579df17cb6bf8eb65bddb3369ab4ed9845a1e918
khushdeep kaur <200431089@student.georgianc.on.ca>
2020-06-06 09:36:19 -0700

    changes for lab 1

--- bash/helloworld.sh
+++ bash/helloworld.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
 # My first script - helloworld.sh
 
 echo 'Hello World!'
-=======
-echo "Hello World!"
->>>>>>> edb9d2c2d03329e00d9d0a55013affff97e7f40d
 echo "I am process # $$"
