eb2c3b7278c2fcc7a9fb8c8a43aaeef1c88706e0
khushdeep kaur <200431089@student.georgianc.on.ca>
2020-07-08 12:11:38 -0700

    lab 2

--- bash/arithmetic-demo.sh
+++ bash/arithmetic-demo.sh
@@ -7,14 +7,17 @@
 #    the sum of the 3 numbers with a label
 #    the product of the 3 numbers with a label
 
-firstnum=5
-secondnum=2
-sum=$((firstnum + secondnum))
-dividend=$((firstnum / secondnum))
-fpdividend=$(awk "BEGIN{printf \"%.2f\", $firstnum/$secondnum}")
+#using read command to get three numbers from user
+read -p "Input1? " number1
+# using read command to get three numbers from user
+read -p "Input2? " number2
+# using read command so that we can get three numbers from user
+read -p "Input3? " number3
 
-cat <<EOF
-$firstnum plus $secondnum is $sum
-$firstnum divided by $secondnum is $dividend
-  - More precisely, it is $fpdividend
-EOF
+# the sum of the 3 numbers with a label
+sum=$((number1 + number2 + number3 ))
+# the product of the 3 numbers with a label
+product=$(( number1 * number2 * number3 ))
+
+echo "$number1 plus $number2 plus $number3 is =$sum"
+echo "$number1 mutiply by $number2 mutiply by $number3 is =$product"
